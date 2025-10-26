3f71a70f326e7cbc20ff0ba68edb6b4fbbe3c564
Albert Bagdasarov <albertbagdasarov85@gmail.com>
2025-10-25 21:16:19 -0400

    new routes

---
+++ backend_api/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from routes.email_reciever_route import router as email_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="MailSense API", version="1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include the single router
+app.include_router(email_router, prefix="/api")
+
+@app.get("/")
+async def root():
+    return {"message": "MailSense API is running 🚀"}
