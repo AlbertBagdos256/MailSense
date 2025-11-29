1d2093cb287b6be48c1b2c6058c7a546e83452d4
Albert Bagdasarov <albertbagdasarov85@gmail.com>
2025-11-28 20:57:48 -0500

    MailSense clusterization and spam detection initial

--- backend_api/utils/spam_detector.py
+++ backend_api/utils/spam_detector.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
 from typing import List
-from utils.email_embedder import embedder
-import joblib
+from huggingface_hub import InferenceClient
+import os
+
+HF_API_KEY = ""
+client = InferenceClient(api_key=HF_API_KEY)
 
 class EmailInput(BaseModel):
     id: str
@@ -9,24 +12,35 @@ class EmailInput(BaseModel):
     body: str
 
 def predict_emails(emails: List[EmailInput]):
-    # Load trained Logistic Regression classifier
-    clf = joblib.load("models/logreg_spam_classifier.joblib")
-    # Concatenate subject + body for embedding
-    texts = [email.body for email in emails]
-
-    # Compute embeddings using preloaded embedder
-    embeddings = embedder.embed_batch(texts)
-
-    # Predict probabilities for spam class
-    spam_probs = clf.predict_proba(embeddings)[:, 1]
-
-    # Apply threshold (e.g., 0.9) to assign labels
     results = []
-    for email, prob in zip(emails, spam_probs):
-        label = "spam" if prob >= 0.9 else "not_spam"
+
+    for email in emails:
+        # Combine subject + body
+        full_text = f"{email.subject}\n{email.body}"
+        
+        # Slice to max 514 characters
+        text = full_text[:350]
+
+        hf_result = client.text_classification(
+            text,
+            model="dima806/email-spam-detection-roberta"
+        )
+
+        # Extract scores
+        spam_score = next((r.score for r in hf_result if r.label.lower() == "spam"), 0.0)
+        no_spam_score = next((r.score for r in hf_result if r.label.lower() in ["no spam", "not spam"]), 0.0)
+
+        # Threshold logic
+        if spam_score >= 0.9991:
+            label = "spam"
+        else:
+            label = "not_spam"
+
         results.append({
             "id": email.id,
             "label": label,
+            "spam_score": spam_score,
+            "no_spam_score": no_spam_score
         })
 
     return results
